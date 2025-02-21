1417f56466e8c7b04371004579aecc3668c59103
A-Karaca <akaraca@student.tgm.ac.at>
2025-02-21 11:17:12 +0100

    GUI basically fertig

--- worttrainer/src/main/java/aKaraca/Main.java
+++ worttrainer/src/main/java/aKaraca/Main.java
@@ -1,33 +1,79 @@
 package aKaraca;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.*;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class Main {
 
 
     public static void main(String[] args) {
-
+        //initial spellingTrainer
         SpellingTrainer spellingTrainer = new SpellingTrainer();
+        String[] currentItem = new String[2];
+        spellingTrainer.randomizeItem();
         try {
-            spellingTrainer.selectItem(6);
+            currentItem = spellingTrainer.getCurrentItem();
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
-        System.out.println(spellingTrainer.getStats());
+
+        //Get Image from URL
+        URL url;
         try {
-            System.out.println(spellingTrainer.getCurrentItem()[0]);
-        } catch (Exception e) {
+            url = new URL(currentItem[1]);
+        } catch (MalformedURLException e) {
             throw new RuntimeException(e);
         }
+        Image image;
         try {
-            spellingTrainer.compareItem("Katze");
-        } catch (Exception e) {
+            image = ImageIO.read(url);
+        } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        System.out.println(spellingTrainer.getStats());
+        image = image.getScaledInstance(600,300,Image.SCALE_DEFAULT);
 
-        JOptionPane.showInputDialog("");
+        //Start of the Game
+        String playerInput = "";
+        playerInput = JOptionPane.showInputDialog(
+                new ImageIcon(image),
+                spellingTrainer.getStats());
 
 
+
+        while(!playerInput.isEmpty()) {
+            try {
+                spellingTrainer.compareItem(playerInput);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+
+            spellingTrainer.randomizeItem();
+            try {
+                currentItem = spellingTrainer.getCurrentItem();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+
+            try {
+                url = new URL(currentItem[1]);
+            } catch (MalformedURLException e) {
+                throw new RuntimeException(e);
+            }
+            try {
+                image = ImageIO.read(url);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            image = image.getScaledInstance(600,300,Image.SCALE_DEFAULT);
+             playerInput = JOptionPane.showInputDialog(
+                    new ImageIcon(image),
+                     spellingTrainer.getStats() + "\nLetzte Eingabe war: " + spellingTrainer.getLastScoreChange());
+
+
+        }
     }
 }
--- worttrainer/src/main/java/aKaraca/SpellingTrainer.java
+++ worttrainer/src/main/java/aKaraca/SpellingTrainer.java
@@ -14,6 +14,8 @@ public class SpellingTrainer {
     // 0: Correct, 1: Wrong, 2: Total
     private int[] playerStats;
     private int listLength;
+    //false: last comparision resulted in a Wrong, true: last comparision resulted in a Correct
+    private boolean lastScore;
     /**
      * Contructor for the Rechtschreibtrainer class
      */
@@ -21,26 +23,27 @@ public class SpellingTrainer {
         listLength = 10;
         wordList = new String[listLength][2];
         wordList[0][0] = "Haus";
-        wordList[0][1] = "URLPlaceholder";
+        wordList[0][1] = "https://www.hanse-haus.de/fileadmin/_processed_/8/b/csm_fertighaus-variant-25-192-hero_967fcf8499.jpg";
         wordList[1][0] = "Auto";
-        wordList[1][1] = "URLPlaceholder";
-        wordList[2][0] = "Schule";
-        wordList[2][1] = "URLPlaceholder";
-        wordList[3][0] = "Stuhl";
-        wordList[3][1] = "URLPlaceholder";
+        wordList[1][1] = "https://image.stern.de/35341676/t/JG/v1/w1440/r1.7778/-/e-auto-elektroauto-vw-id-7-pro-s.jpg";
+        wordList[2][0] = "Baum";
+        wordList[2][1] = "https://biberberti.com/wp-content/uploads/2021/08/04-px-alter-Baum.jpg";
+        wordList[3][0] = "Laptop";
+        wordList[3][1] = "https://gfx3.senetic.com/akeneo-catalog/f/6/e/6/f6e64a862fe63da6a23c2f90d2e0ed5f3f3a1775_1626671_5V8_00009_image1.jpg";
         wordList[4][0] = "Tisch";
-        wordList[4][1] = "URLPlaceholder";
+        wordList[4][1] = "https://www.steinershopping.at/media/mf_webp/jpg/media/catalog/product/cache/3c0b12cec63083e386fe3f6df73f2f21/vv_img/2/4/240_abc_1_1_1.webp";
         wordList[5][0] = "Hund";
-        wordList[5][1] = "URLPlaceholder";
+        wordList[5][1] = "https://www.tierschutzbund.de/fileadmin/_processed_/7/c/csm_schwarzer_Hund_auf_Wiese_c_xkunclova-Shutterstock_01_5566a80d25.jpg";
         wordList[6][0] = "Katze";
-        wordList[6][1] = "URLPlaceholder";
+        wordList[6][1] = "https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Cat_August_2010-4.jpg/640px-Cat_August_2010-4.jpg";
         wordList[7][0] = "Maus";
-        wordList[7][1] = "URLPlaceholder";
+        wordList[7][1] = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSeATPVhMq73dqTDjrg3kGpyISJOcm7VOxyFw&s";
         wordList[8][0] = "Buch";
-        wordList[8][1] = "URLPlaceholder";
+        wordList[8][1] = "https://wasliestdu.de/dateien/styles/680x380/public/artikel/die-leere-nach-einem-guten-buch.jpg?itok=JmYo77_N";
         wordList[9][0] = "Stift";
-        wordList[9][1] = "URLPlaceholder";
+        wordList[9][1] = "https://einguterplan.de/wp-content/uploads/2024/08/EGStift-farbiger-Hintergrund-1024x1024.jpg";
 
+        lastScore = false;
 
         playerStats = new int[3];
         playerStats[0] = 0;
@@ -81,8 +84,10 @@ public class SpellingTrainer {
         }
         if(wordList[selectedIndex][0].equals(playerInput)){
             playerStats[0]++;
+            lastScore = true;
         }else{
             playerStats[1]++;
+            lastScore = false;
         }
         playerStats[2]++;
     }
@@ -110,5 +115,9 @@ public class SpellingTrainer {
         playerStats[0] = 0;
         playerStats[1] = 0;
         playerStats[2] = 0;
+    }
+
+    public boolean getLastScoreChange(){
+        return lastScore;
     }
 }
